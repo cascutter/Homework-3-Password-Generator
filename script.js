2284f6f09d2deaa3f145e08354133cf9a57d42cc
cascutter <cascutter13@gmail.com>
2020-08-04 11:52:59 -0500

    Created generator functions for upper, lower, number, and symbol

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Control password length
+// document.getElementById("length").innerHTML = "Length: 25";
+
+// Functions for generator
+
+// Generate random uppercase letter
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+} 
+// Generate random lowercase letter
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// Generate random number
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// Generate random symbol
+function randomSymbol() {
+  const symbol = "!#$%&'()*+,-./:;<=>?@[]{|}~";
+  return symbol[Math.floor(Math.random() * symbol.length)];
+  }
+
+// Test
+console.log (randomUpper());
